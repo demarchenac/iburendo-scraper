d176827098fc66403eb4d95d4c4617abbdb48b2d
Cristhyan D. Marchena <crdemar@gmail.com>
2024-09-15 18:50:26 -0500

    feat(app): added more env variables

--- src/env.ts
+++ src/env.ts
@@ -6,8 +6,12 @@ export const env = createEnv({
   emptyStringAsUndefined: true,
   server: {
     AZURE_FILE_STORAGE_SAS_TOKEN: z.string().min(1),
+    AZURE_FILE_STORAGE_ACCOUNT_NAME: z.string().min(1),
+    AZURE_FILE_STORAGE_CONTAINER_NAME: z.string().min(1),
   },
   runtimeEnvStrict: {
     AZURE_FILE_STORAGE_SAS_TOKEN: process.env.AZURE_FILE_STORAGE_SAS_TOKEN,
+    AZURE_FILE_STORAGE_ACCOUNT_NAME: process.env.AZURE_FILE_STORAGE_ACCOUNT_NAME,
+    AZURE_FILE_STORAGE_CONTAINER_NAME: process.env.AZURE_FILE_STORAGE_CONTAINER_NAME,
   },
 });
